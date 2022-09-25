f55f480525857765004af14698499cb249466b79
Jake Son <sonjeabin@gmail.com>
2022-09-25 18:14:21 +0900

    fix: change exe path

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 let binary;
 if (process.platform === 'darwin' && process.arch === 'x64') {
-  binary = path.join(__dirname, 'macos-x64', 'react-query-generator');
+  binary = path.join(__dirname, 'macos-x64', 'generator');
 } else if (process.platform === 'darwin' && process.arch === 'arm64') {
-  binary = path.join(__dirname, 'macos-arm64', 'react-query-generator');
+  binary = path.join(__dirname, 'macos-arm64', 'generator');
 } else if (process.platform === 'linux' && process.arch === 'x64') {
-  binary = path.join(__dirname, 'linux-x64', 'react-query-generator');
+  binary = path.join(__dirname, 'linux-x64', 'generator');
 } else if (process.platform === 'linux' && process.arch === 'arm64') {
-  binary = path.join(__dirname, 'linux-arm64', 'react-query-generator');
+  binary = path.join(__dirname, 'linux-arm64', 'generator');
 } else if (process.platform === 'win32' && process.arch === 'x64') {
-  binary = path.join(__dirname, 'win-x64', 'react-query-generator.exe');
+  binary = path.join(__dirname, 'win-x64', 'generator.exe');
 } else {
   binary = null;
 }
